89dd59bec600cb316f4457a3f6562c9e5893e80d
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-19 19:04:39 -0300

    feat: Criando classe abstrata Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,20 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private _special = 0;
+  private _cost = 0;
+
+  constructor(private _name: string) {}
+
+  public get name(): string { return this._name; }
+
+  public get special(): number { return this._special; }
+
+  public get cost(): number { return this._cost; }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
